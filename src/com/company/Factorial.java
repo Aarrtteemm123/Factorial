84137e9bf03ce81a28f25d820e37d979b984e05e
Artem <artem@gmail.com>
2019-10-05 12:28:55 +0300

    Initial commit

---
+++ src/com/company/Factorial.java
@@ -0,0 +1,139 @@
+package com.company;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Factorial {
+    int number;
+    double factorial[] = new double[10];
+
+    public Factorial() {
+    }
+
+    public Factorial(int number) {
+        if (number >= 0)
+            this.number = number;
+        factorial[factorial.length - 1] = 1;
+    }
+
+    public void calculationFactorial() {
+
+        for (int i = 1; i <= number; i++) {
+            multiplicationArrOnNum(i);
+        }
+        deleteFrontZero();
+    }
+
+    private double[] multiplicationArrOnNum(int num) {
+        for (int i = factorial.length - 1; i > -1; i--)
+            factorial[i] *= num;
+        factorial = checkOverflow(factorial);
+        return factorial;
+    }
+
+
+    private double[] checkOverflow(double arr[]) {
+        for (int i = arr.length - 1; i > -1; i--) {
+            if (arr[i] > 9 && i != 0) {
+                int remainder = (int) (arr[i] % 10);
+                arr[i - 1] += (arr[i] - remainder) / 10;
+                arr[i] = remainder;
+            }
+            if (arr[i] > 9 && i == 0) {
+                arr = arrResize(arr, (int) (1.5 * arr.length));
+                i = arr.length - 1;
+            }
+        }
+        return arr;
+    }
+
+    private double[] arrResize(double arr[], int newLenght) {
+        if (newLenght > arr.length) {
+            double  bufferArr[] = new double [newLenght];
+            int indexElement = bufferArr.length - 1;
+            for (int i = arr.length - 1; i > -1; i--) {
+                bufferArr[indexElement] = arr[i];
+                indexElement--;
+            }
+            return bufferArr;
+        }
+        double  bufferArr[] = new double [newLenght];
+        int indexElement = arr.length - 1;
+        for (int i = bufferArr.length - 1; i > -1; i--) {
+            bufferArr[i] = arr[indexElement];
+            indexElement--;
+        }
+        return bufferArr;
+
+    }
+
+    private void deleteFrontZero() {
+        int i = 0;
+        while (factorial[i] == 0 && i < factorial.length - 1)
+            i++;
+        if (i != 0)
+            factorial = arrResize(factorial, factorial.length - i);
+    }
+
+    private void clearFactorialArr() {
+        for (int i = 0; i < factorial.length; i++)
+            factorial[i] = 0;
+    }
+
+    public double [] getFactorial() {
+        return factorial;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        if (number >= 0)
+            this.number = number;
+    }
+
+    public void print() {
+        for (int i = 0; i < factorial.length; i++) {
+            if (i % 100 == 0 && i != 0)
+                System.out.println();
+            System.out.print((int) factorial[i]);
+        }
+        System.out.println("\n\nSize(num) : " + factorial.length);
+        System.out.println("Lenght(m) : " + (double) factorial.length * 2.5 / 1000);
+        // normal size font in console
+    }
+
+    public void save(String fileName)
+    {
+        try(FileWriter writer = new FileWriter(fileName,false)) {
+            for (int i = 0; i < factorial.length; i++)
+            {
+                if (i%110==0&&i!=0)
+                    writer.write("\n");
+                writer.write(String.valueOf((int) factorial[i]));
+            }
+            writer.write("\n\nSize(num) : " + factorial.length);
+            writer.write("\nLenght(m) : " + (double) factorial.length * 2.5 / 1000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save(String fileName,double time)
+    {
+        try(FileWriter writer = new FileWriter(fileName,false)) {
+            for (int i = 0; i < factorial.length; i++)
+            {
+                if (i%110==0&&i!=0)
+                    writer.write("\n");
+                writer.write(String.valueOf((int) factorial[i]));
+            }
+            writer.write("\n\nSize(num) : " + factorial.length);
+            writer.write("\nLenght(m) : " + (double) factorial.length * 2.5 / 1000);
+            writer.write("\nTime(s) : "+(time)/1000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,182 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.concurrent.Phaser;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int n = 10000;            // number
+        int countThreads = 4;    // threads
+        String fileName = "Result.txt";
+
+
+        ArrayList<double[]>arrayList=new ArrayList<>(countThreads);
+        switch (countThreads)
+        {
+            case 1:
+            {
+                double start=System.currentTimeMillis();
+                Factorial factorial = new Factorial(n);
+                factorial.calculationFactorial();
+                factorial.print();
+                double finish=System.currentTimeMillis();
+                factorial.save(fileName,finish-start);
+                System.out.println("Time(s) : "+(finish-start)/1000);
+                break;
+            }
+            case 2:
+            {
+                Phaser phaser=new Phaser(1);
+                double start=System.currentTimeMillis();
+
+                CalculationFactorial []threads = new CalculationFactorial[2];
+                for (int i = 0; i < threads.length; i++)
+                    threads[i] = new CalculationFactorial(n, i+1, countThreads,phaser,arrayList,fileName);
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].start();
+
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndDeregister();
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].join();
+
+                double finish=System.currentTimeMillis();
+
+                System.out.println("Time(s) : "+(finish-start)/1000);
+                break;
+            }
+            case 4:
+            {
+                Phaser phaser=new Phaser(1);
+                double start=System.currentTimeMillis();
+
+                CalculationFactorial []threads = new CalculationFactorial[4];
+                for (int i = 0; i < threads.length; i++)
+                    threads[i] = new CalculationFactorial(n, i+1, countThreads,phaser,arrayList,fileName);
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].start();
+
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndDeregister();
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].join();
+
+                double finish=System.currentTimeMillis();
+                System.out.println("Time(s) : "+(finish-start)/1000);
+                break;
+            }
+            case 8:
+            {
+                Phaser phaser=new Phaser(1);
+                double start=System.currentTimeMillis();
+
+                CalculationFactorial []threads = new CalculationFactorial[8];
+                for (int i = 0; i < threads.length; i++)
+                    threads[i] = new CalculationFactorial(n, i+1, countThreads,phaser,arrayList,fileName);
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].start();
+
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndDeregister();
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].join();
+
+                double finish=System.currentTimeMillis();
+                System.out.println("Time(s) : "+(finish-start)/1000);
+                break;
+            }
+            case 16:
+            {
+                Phaser phaser=new Phaser(1);
+                double start=System.currentTimeMillis();
+
+                CalculationFactorial []threads = new CalculationFactorial[16];
+                for (int i = 0; i < threads.length; i++)
+                    threads[i] = new CalculationFactorial(n, i+1, countThreads,phaser,arrayList,fileName);
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].start();
+
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndDeregister();
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].join();
+
+                double finish=System.currentTimeMillis();
+                System.out.println("Time(s) : "+(finish-start)/1000);
+                break;
+            }
+            case 32:
+            {
+                Phaser phaser=new Phaser(1);
+                double start=System.currentTimeMillis();
+
+                CalculationFactorial []threads = new CalculationFactorial[32];
+                for (int i = 0; i < threads.length; i++)
+                    threads[i] = new CalculationFactorial(n, i+1, countThreads,phaser,arrayList,fileName);
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].start();
+
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndDeregister();
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].join();
+
+
+                double finish=System.currentTimeMillis();
+                System.out.println("Time(s) : "+(finish-start)/1000);
+                break;
+            }
+            case 64:
+            {
+                Phaser phaser=new Phaser(1);
+                double start=System.currentTimeMillis();
+
+                CalculationFactorial []threads = new CalculationFactorial[64];
+                for (int i = 0; i < threads.length; i++)
+                    threads[i] = new CalculationFactorial(n, i+1, countThreads,phaser,arrayList,fileName);
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].start();
+
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndAwaitAdvance();
+                phaser.arriveAndDeregister();
+
+                for (int i = 0; i < threads.length; i++)
+                    threads[i].join();
+
+
+                double finish=System.currentTimeMillis();
+                System.out.println("Time(s) : "+(finish-start)/1000);
+                break;
+            }
+        }
+
+    }
+}
